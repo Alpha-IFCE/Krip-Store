10b88695112ca7293b5e61b45f2c14410ddfd76d
Luiz Gabriel Ribeiro <bieel180360@gmail.com>
2022-11-03 02:59:00 -0300

    novo design pro site

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-const client = require('../client')
+const client = require("../client");
 
 router.get("/", function (req, res, next) {
   getUsers(client).then((users) => console.log(JSON.stringify(users)));
   if (req.cookies["AuthToken"]) {
     res.render("index", {
-      messageTitle: "Usuário logado",
-      welcomeMessage: "Bem vindo a nossa loja!",
-      messageIndex: "Logout",
-      hrefI: "/logout",
+      LoginBtnMsg: "Logout",
+      loginBtnLink: "/logout"
     });
   } else {
     res.render("index", {
-      messageTitle: "Faça login",
-      welcomeMessage: "Bem vindo a nossa loja! Faça login para acessar outras páginas.",
-      messageIndex: "Faça login",
-      hrefI: "/login",
+      LoginBtnMsg: "Login",
+      loginBtnLink: "/login"
     });
   }
 });
