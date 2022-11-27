ee9ade7f4398b7424d7783e55de6cd43118c8139
Caio <caiogrebr@gmail.com>
2022-11-27 13:32:44 -0300

    usando a api

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,52 @@
+const { response } = require("express");
 var express = require("express");
 var router = express.Router();
 
 const client = require("../client");
 
-const camisas = require('../public/scripts/camisas')
+
+const getData = async() => {
+    const products = await getProducts();
+    const camisas = await getCamisas();
+
+    return { products, camisas }
+}
+
 
 router.get("/", function (req, res, next) {
-  getUsers(client).then((users) => console.log(JSON.stringify(users)));
-  if (req.cookies["AuthToken"]) {
-    res.render("index", {
-      LoginBtnMsg: "Logout",
-      loginBtnLink: "/logout",
-      camisas
-    });
-  } else {
-    res.render("index", {
-      LoginBtnMsg: "Login",
-      loginBtnLink: "/login",
-      camisas
-    });
-  }
+    
+    getCamisas().then((camisas) => {
+        if (req.cookies["AuthToken"]) {
+            res.render("index", {
+                LoginBtnMsg: "Logout",
+                loginBtnLink: "/logout",
+                camisas
+            });
+        } else {
+            res.render("index", {
+                LoginBtnMsg: "Login",
+                loginBtnLink: "/login",
+                camisas
+            });
+        }
+    })
+    
 });
 
-async function getUsers(client) {
-  try {
-    await client.connect();
-    return await client.db("auth").collection("users").find().toArray();
-  } finally {
-    await client.close();
-  }
+const getCamisas = async() => {
+    const response = await fetch('http://localhost:8080/categories/camisa/products')
+    const data = await response.json()
+    // console.log(data.data)
+    return data.data
 }
+
+// async function getUsers(client) {
+//   try {
+//     await client.connect();
+//     return await client.db("auth").collection("users").find().toArray();
+//   } finally {
+//     await client.close();
+//   }
+// }
 
 module.exports = router;
