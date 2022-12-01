ea44c5fcb64a95cd1d1cbdef0cfc26a81282d34a
jhonnycs <jhonnycunha7@gmail.com>
2022-11-30 22:15:05 -0300

    mexendo com api de frete

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,24 @@ var router = express.Router();
 const client = require("../client");
 
 router.get("/", async (req, res, next) => {
-    const produtos = await getProdutos()
+    const produtos = await getProdutos();
 
-    const produtosForCarousel = produtos.slice(0, 6)
+    const produtosForCarousel = produtos.slice(0, 6);
 
-    const user = global.authTokens[req.cookies['AuthToken']]
+    const user = global.authTokens[req.cookies["AuthToken"]];
 
     res.render("index", {
         user,
-        produtos: produtosForCarousel
+        produtos: produtosForCarousel,
     });
 });
 
-const getProdutos = async() => {
-    const response = await fetch('http://localhost:8080/produtos')
-    const data = await response.json()
+const getProdutos = async () => {
+    const response = await fetch("http://localhost:8080/produtos");
+    const data = await response.json();
     // console.log(data.data)
-    return data
-}
+    return data;
+};
 
 // async function getUsers(client) {
 //   try {
--- routes/product-page.js
+++ routes/product-page.js
@@ -1,23 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const frete = require("frete");
 
-router.get('/:produtoId', async(req, res) => {
+router.get("/:produtoId", async (req, res) => {
     const produto = await getProduto(req.params.produtoId);
 
-    const user = global.authTokens[req.cookies['AuthToken']]
+    const user = global.authTokens[req.cookies["AuthToken"]];
 
-    res.render('product-page', {
+    res.render("product-page", {
         user,
-        produto
-    })
-
+        produto,
+    });
 });
 
-const getProduto = async(produtoId) => {
-    const response = await fetch(`http://localhost:8080/produto/${produtoId}`)
-    const data = await response.json()
-    console.log(data)
-    return data
-}
+const getProduto = async (produtoId) => {
+    const response = await fetch(`http://localhost:8080/produto/${produtoId}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+};
+
+router.post("/:produtoId", async (req, res) => {
+    const produto = await getProduto(req.params.produtoId);
+
+    const user = global.authTokens[req.cookies["AuthToken"]];
+
+    const cep = req.body.cep;
+
+    frete()
+        .cepOrigem(cep)
+        .peso(1)
+        .formato(frete.formatos.caixaPacote)
+        .comprimento(16)
+        .altura(2)
+        .largura(11)
+        .diametro(1)
+        //.maoPropria(frete.maoPropria.nao)
+        .valorDeclarado(50)
+        //.avisoRecebimento(frete.avisoRecebimento.sim)
+        .servico(frete.servicos.sedex)
+        .precoPrazo("13466321", function (err, results) {
+            console.log("erro: ", err);
+            console.log(results);
+        });
+
+    res.render("product-page", {
+        user,
+        produto,
+    });
+});
 
 module.exports = router;
